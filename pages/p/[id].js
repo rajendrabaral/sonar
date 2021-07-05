5c901a1b5b7d7818dca0e4c570129bed4ba5891b
rajendrabaral <84062261+rajendrabaral@users.noreply.github.com>
2021-07-05 14:21:35 +0530

    initial push

---
+++ pages/p/[id].js
@@ -0,0 +1,23 @@
+import Layout from '../../components/MyLayout';
+import fetch from 'isomorphic-unfetch';
+
+const Post = props => (
+  <Layout>
+    <h1>{props.show.name}</h1>
+    <p>{props.show.summary.replace(/<[/]?p>/g, '')}</p>
+    {props.show.image && props.show.image.medium && <img src={props.show.image.medium} />}
+
+  </Layout>
+);
+
+Post.getInitialProps = async function(context) {
+  const { id } = context.query;
+  const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
+  const show = await res.json();
+
+  console.log(`Fetched show: ${show.name}`);
+
+  return { show };
+};
+
+export default Post;
